5363c2ed2c70f59d8dbc5ff83b25f75a0ff845b7
kamiladzimira <dzimirakamila@gmail.com>
2024-07-16 19:24:04 +0200

    Project optimization.

--- TasksFromKoU/Assets/Zadanie Grawitacja/Scripts/GravityBallForceController.cs
+++ TasksFromKoU/Assets/Zadanie Grawitacja/Scripts/GravityBallForceController.cs
@@ -7,23 +7,25 @@ public class GravityBallForceController : MonoBehaviour
     [SerializeField]
     private float _g;
 
-    public float G => _g;
-
     private void FixedUpdate()
     {
         for (int i = 0; i < _ballSpawner.GravityBalls.Count; i++)
         {
-            for (int j = 0; j < _ballSpawner.GravityBalls.Count; j++)
+            for (int j = i + 1; j < _ballSpawner.GravityBalls.Count; j++)
             {
-                if (i == j || !_ballSpawner.GravityBalls[i].BallCollider.isActiveAndEnabled || !_ballSpawner.GravityBalls[j].BallCollider.isActiveAndEnabled)
+                if (i == j || !_ballSpawner.GravityBalls[i].IsActive || !_ballSpawner.GravityBalls[j].IsActive)
                 {
                     continue;
                 }
                 float g = _ballSpawner.ForceType == ForceType.Attract ? _g : -_g;
                 Vector2 direction = (_ballSpawner.GravityBalls[j].transform.position - _ballSpawner.GravityBalls[i].transform.position).normalized;
-                float force = CalculateGravitationalForces(_ballSpawner.GravityBalls[i].BallRigidbody.mass, _ballSpawner.GravityBalls[j].BallRigidbody.mass, g, 
-                    Vector2.Distance(_ballSpawner.GravityBalls[i].transform.position, _ballSpawner.GravityBalls[j].transform.position));
-                _ballSpawner.GravityBalls[i].BallRigidbody.AddForce(direction * force);
+                float distanceBetweenBalls = Vector2.Distance(_ballSpawner.GravityBalls[i].transform.position, _ballSpawner.GravityBalls[j].transform.position);
+                float forcePower = distanceBetweenBalls > Mathf.Epsilon ? CalculateGravitationalForces(_ballSpawner.GravityBalls[i].BallRigidbody.mass, _ballSpawner.GravityBalls[j].BallRigidbody.mass, g,
+                    distanceBetweenBalls) : 1;
+
+                Vector2 force = direction * forcePower;
+                _ballSpawner.GravityBalls[i].BallRigidbody.AddForce(force);
+                _ballSpawner.GravityBalls[j].BallRigidbody.AddForce(-1 * force);
             }
         }
     }
